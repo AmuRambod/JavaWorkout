f711a705ef3c544afd061ee7e6978debe43180e1
Rambod Rahimlou <rambod@pop-os.localdomain>
2021-09-03 17:54:40 +0430

    DataTable update
    added a menu for showData and weeklyWeightTable feature

--- DataTable.java
+++ DataTable.java
@@ -2,8 +2,31 @@ import java.sql.*;
 
 public class DataTable {
     public static void showData() {
-        // i have to move it over to database connection
-        // String getDataQuery = "SELECT * FROM daily_weight;";
+        CommandLineTable dataTable = new CommandLineTable();
+        String showDataMenu = "\n *** SHOW DATA ***\n\n" + "1. Daily weight\n" + "2. Weekly weight\n"
+                + "\nEnter your choice: ";
+        System.out.print(showDataMenu);
+        char choice = main.keyboard.nextLine().charAt(0);
+        System.out.println("--------------");
+        switch (choice) {
+            case '1':
+                dataTable = getDailyWeightTable();
+                System.out.println("Daily Weight Table:");
+                break;
+            case '2':
+                dataTable = getWeeklyWeightTable();
+                System.out.println("Weekly Weight Table:");
+                break;
+            default:
+                dataTable = null;
+                System.out.println("Choice not implemented!");
+                break;
+        }
+        dataTable.print();
+    }
+
+    public static CommandLineTable getDailyWeightTable() {
+        String getDataQuery = "SELECT * FROM daily_weight;";
         CommandLineTable dataTable = new CommandLineTable();
         dataTable.setShowVerticalLines(true);
         dataTable.setHeaders("Date", "Week day", "Weight");
@@ -12,7 +35,6 @@ public class DataTable {
             Statement myStatement = DatabaseConnection.connection.createStatement();
             ResultSet myResultSet = myStatement.executeQuery(getDataQuery);
             while (myResultSet.next()) {
-                // System.out.println(myResultSet.getDate("Date").toString(),myResultSet.getByte("day_of_week"),myResultSet.getFloat("weight"));
                 switch (myResultSet.getByte("day_of_week")) {
                     case 0:
                         weekDay = "Saturday";
@@ -41,12 +63,32 @@ public class DataTable {
                 }
                 dataTable.addRow(myResultSet.getDate("Date").toString(), weekDay,
                         Float.toString(myResultSet.getFloat("weight")));
-                // String kir = myResultSet.getFloat("weight").toString();
             }
-            dataTable.print();
+            // dataTable.print();
+            return dataTable;
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            return null;
         }
-        // dataTable.print();
+    }
+
+    public static CommandLineTable getWeeklyWeightTable() {
+        String getWeeklyWeightQuery = "SELECT * FROM weekly_weight;";
+        CommandLineTable weeklyWeightTable = new CommandLineTable();
+        weeklyWeightTable.setShowVerticalLines(true);
+        weeklyWeightTable.setHeaders("Start", "End", "Average Weight");
+        try {
+            Statement getWeeklyWeightStatement = DatabaseConnection.connection.createStatement();
+            ResultSet weeklyWeightResultSet = getWeeklyWeightStatement.executeQuery(getWeeklyWeightQuery);
+            while (weeklyWeightResultSet.next()) {
+                weeklyWeightTable.addRow(weeklyWeightResultSet.getDate("week_start").toString(),
+                        weeklyWeightResultSet.getDate("week_end").toString(),
+                        Float.toString(weeklyWeightResultSet.getFloat("avg_weight")));
+            }
+            return weeklyWeightTable;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 }
